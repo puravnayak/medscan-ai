83e920a12fbad931a6edcb9c8aef2ab8fca0b6e7
puravnayak <143780759+puravnayak@users.noreply.github.com>
2025-06-25 12:47:25 +0000

    downloader fix

--- backend/app/core/downloader.py
+++ backend/app/core/downloader.py
@@ -1,6 +1,8 @@
 import os
 import gdown
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) 
+
 file_ids = {
     "models/logistic_regression.pkl": "1FcoXQBXkOFXzKkwm5Z1tz_vcqdoCzvhe",
     "models/svm.pkl": "16POS1zHpfypNP57JePz-Bafrh-OYGxdd",
@@ -9,8 +11,11 @@ file_ids = {
 }
 
 def download_missing_models():
-    os.makedirs("models", exist_ok=True)
-    for path, file_id in file_ids.items():
-        if not os.path.exists(path):
-            print(f"Downloading {path}...")
-            gdown.download(id=file_id, output=path, quiet=False)
+    model_dir = os.path.join(BASE_DIR, "models")
+    os.makedirs(model_dir, exist_ok=True)
+
+    for rel_path, file_id in file_ids.items():
+        full_path = os.path.join(BASE_DIR, rel_path)
+        if not os.path.exists(full_path):
+            print(f"Downloading {rel_path}...")
+            gdown.download(id=file_id, output=full_path, quiet=False)
